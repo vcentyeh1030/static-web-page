b47735301111074d8418a15f12f0ffb125fc9931
VincentYeh <vincent53302172@gmail.com>
2019-04-14 23:15:22 +0800

    transform test

---
+++ script.js
@@ -0,0 +1,9 @@
+;
+(function() {})();
+
+// document.querySelectorAll('.panel').forEach(function(panel) {
+
+//   panel.addEventListener('click', function() {
+//     this.classList.toggle('active');
+//   });
+// });
